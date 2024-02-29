5e2fb416a682be92531eee83a17885334ebc2131
Sud0 <161523917+Sud-0@users.noreply.github.com>
2024-02-29 00:38:49 -0800

    Create random guns.py

---
+++ zero hour/random guns.py
@@ -0,0 +1,17 @@
+import keyboard
+import random
+
+MainGuns = ['SR 7.62','tactical shotgun','M4','Ballitsic shield','Oppresor','ES36','UMP','Rattler','HB82','Mark 12','Mac-10','Kyanite','AKU','Sawed off','Bizon','Valkyrie','FAL','SKS']
+Sidearms = ['Mock 17','P10C','Falcon','SW11','FN57']
+Tools1 = ['Frag grenade','Flash grenade','C2 Charge','Night vision']
+Tools2 = ['Frag grenade','Flash grenade','UDC','Smoke grenade']
+Tools3 = ['Door rammer','Medkit']
+
+while True:
+    keyboard.wait('e')
+    print('--------------')
+    print(random.choice(MainGuns))
+    print(random.choice(Sidearms))
+    print(random.choice(Tools1))
+    print(random.choice(Tools2))
+    print(random.choice(Tools3))
