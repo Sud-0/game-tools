8b09c451ab12586a4cc700b342a56f4616d3be7c
Sud0 <161523917+Sud-0@users.noreply.github.com>
2024-02-29 00:54:48 -0800

    Update random guns.py

--- zero hour/random guns.py
+++ zero hour/random guns.py
@@ -1,6 +1,7 @@
 import keyboard
 import random
 
+#this program in no way interacts with the game, to avoid being a cheat, it simply prints names to the console
 MainGuns = ['SR 7.62','tactical shotgun','M4','Ballitsic shield','Oppresor','ES36','UMP','Rattler','HB82','Mark 12','Mac-10','Kyanite','AKU','Sawed off','Bizon','Valkyrie','FAL','SKS']
 Sidearms = ['Mock 17','P10C','Falcon','SW11','FN57']
 Tools1 = ['Frag grenade','Flash grenade','C2 Charge','Night vision']
